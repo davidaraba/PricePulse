cc7181f6b4496b5239a3964c152ece32f018ba90
David Araba <daviidaraba@outlook.com>
2025-06-29 17:07:36 +1000

    Implemented logic to track oringal & sale price as well as % discount

--- scrapers/nike.py
+++ scrapers/nike.py
@@ -6,15 +6,40 @@ def scrape_nike_store(url: str, keywords: list[str]) -> list[dict]:
     response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, "html.parser")
-    name_tags = soup.find_all("div", class_="product-card__title")
+    product_cards = soup.find_all("div", class_="product-card__info")
     matches = []
 
-    for tag in name_tags:
-        name = tag.get_text(strip=True)
+    for card in product_cards:
+        # print(tag.prettify())
+        # break
+        name_tag = card.find("div", class_="product-card__title")
+        name = name_tag.get_text(strip=True) if name_tag else ""
+        
+        original_price = card.find("div", class_="product-price au__styling is--striked-out css-0")
+        sale_price = card.find("div", class_= "product-price is--current-price css-1mj7kho")
+
+        if sale_price:
+            sale_price = float(sale_price.get_text(strip=True).replace("$", ""))
+        else:
+            sale_price = None
+        
+        if original_price:
+            original_price = float(original_price.get_text(strip=True).replace("$", ""))
+        else:
+            original_price = sale_price 
+
+        if sale_price and original_price and sale_price < original_price:
+            discount = round((1 - sale_price / original_price) * 100)
+        else:
+            discount = 0
+        
         for kw in keywords:
             if kw.lower() in name.lower():
                 matches.append({
                     "name" : name,
-                    "tag" : tag
+                    "original price" : original_price, 
+                    "sale price" : sale_price,
+                    "discount percentage" : discount,
+                    "card" : card
                 })
     return matches

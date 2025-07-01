da8b5eb9320dd2f8b833aa6f3d52cea4d96d5160
David Araba <daviidaraba@outlook.com>
2025-07-01 12:15:02 +1000

    Scraper now extracts names from website

--- scrapers/cottonon.py
+++ scrapers/cottonon.py
@@ -4,4 +4,36 @@ from core.product import Product
 
 
 def scrape_cottonon_store(url: str, keywords: list[str], max_price: float = None) -> list[Product]:
-    pass
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    cottonon_product = soup.find_all("div", class_="product-name")
+    seen_names = set()
+    matches = []
+    
+    for product in cottonon_product:
+        name_tag = product.find("a", class_="name-link")
+        name = name_tag.get_text(strip=True) if name_tag else ""
+
+        if name in seen_names:
+            continue
+        
+    
+        for kw in keywords:
+            if kw.lower() in name.lower():
+                seen_names.add(name)
+                cottonon_product = Product(
+                    name=name,
+                    sale_price="1",
+                    original_price="1",
+                    discount_percentage="1",
+                    store="Cotton On",
+                    link="david"
+                )
+                matches.append(cottonon_product)
+                break
+    
+    return matches
+
+

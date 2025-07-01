6a6223e78a1712e322c13d7a1042fbf5b62f6aa4
David Araba <daviidaraba@outlook.com>
2025-07-01 12:41:15 +1000

    Implemented function to extract price

--- scrapers/cottonon.py
+++ scrapers/cottonon.py
@@ -8,7 +8,7 @@ def scrape_cottonon_store(url: str, keywords: list[str], max_price: float = None
     response = requests.get(url, headers=headers)
     
     soup = BeautifulSoup(response.text, "html.parser")
-    cottonon_product = soup.find_all("div", class_="product-name")
+    cottonon_product = soup.find_all("div", class_="product-tile")
     seen_names = set()
     matches = []
     
@@ -16,32 +16,44 @@ def scrape_cottonon_store(url: str, keywords: list[str], max_price: float = None
         name_tag = product.find("a", class_="name-link")
         name = name_tag.get_text(strip=True) if name_tag else ""
 
-        link_tag = product.find("a", href = True)
-
-        if link_tag:
-            link = link_tag.get("href")
-        else:
-            link = ""
-
-
         if name in seen_names:
             continue
         
-    
+        link_tag = product.find("a", href = True)
+        link = link_tag.get("href") if link_tag else ""
+ 
+        original_price = product.find("span", class_="product-standard-price")
+        sale_price = product.find("span", class_="product-sales-price")
+
+        original_price = extract_price(original_price) or extract_price(sale_price)
+        sale_price = extract_price(sale_price)
+
+        
         for kw in keywords:
             if kw.lower() in name.lower():
                 seen_names.add(name)
-                cottonon_product = Product(
+                co_product = Product(
                     name=name,
-                    sale_price="1",
-                    original_price="1",
+                    sale_price=sale_price,
+                    original_price=original_price,
                     discount_percentage="1",
                     store="Cotton On",
                     link=link
                 )
-                matches.append(cottonon_product)
+                matches.append(co_product)
                 break
     
     return matches
 
+
+
+def extract_price(tag) -> float:
+    if not tag:
+        return None
+    text = tag.get_text(strip=True).replace("AUD", "").replace("$", "").strip()
+    try:
+        return float(text)
+    except ValueError:
+        return None
+    
 

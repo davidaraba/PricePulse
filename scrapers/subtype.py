1106528b7051bac6b7c32b281a876cf7af4785cc
David Araba <daviidaraba@outlook.com>
2025-07-02 15:33:32 +1000

    Inital setup for subtype scraper

--- scrapers/subtype.py
+++ scrapers/subtype.py
@@ -1,9 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from core.product import Product
-import requests
-from bs4 import BeautifulSoup
+from typing import List
 
-def scrape_subtype_store (url: str, keywords: list[str], max_price: float = None) -> list[Product]:
-    pass
+def scrape_subtype_store(url: str, keywords: list[str], max_price: float = None) -> List[Product]:
+    options = Options()
+    options.add_argument("--headless")  # run browser in headless mode
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
 
-response = requests.get("https://www.subtypestore.com/au/categories/sale/mens-sale/mens-sale-footwear?pid=nav-sale-mens")
-print(response)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get("https://google.com")
+
+    matches = []
+
+    # driver.quit()
+    return matches
